d38c1682d4b44440e3db0d404560b352d931baa2
Rohit Paul Gerard Nagarajan <gerardnagarajan.r@northeastern.edu>
2023-10-25 01:29:29 -0400

    Commit #1- Pushing assets

---
+++ Dream Board/Dream Board/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Dream Board
+//
+//  Created by Rohit Paul on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Dream Board/Dream Board/Dream_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dream_BoardApp.swift
+//  Dream Board
+//
+//  Created by Rohit Paul on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Dream_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
